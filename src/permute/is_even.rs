5ee7fef9935ffa18b89c79774693b9310fac4f97
Travis Vanderstad <t.vanderstad@gmail.com>
2020-11-22 12:49:22 -0500

    permutation is even tests

--- src/permute/is_even.rs
+++ src/permute/is_even.rs
@@ -5,3 +5,32 @@ use crate::permute::Permutation;
 pub fn is_even(p: &Permutation) -> bool {
     laplace_expansion(&Matrix::from(p)) == 1
 }
+
+#[cfg(test)]
+mod test {
+    use super::is_even;
+    use crate::permute::Permutation;
+
+    #[test]
+    fn ordered() {
+        for i in 0..6 {
+            assert!(is_even(&Permutation::new(i)));
+        }
+    }
+
+    #[test]
+    fn two() {
+        assert!(is_even(&Permutation{ v: vec![0, 1] }));
+        assert!(!is_even(&Permutation{ v: vec![1, 0] }));
+    }
+
+    #[test]
+    fn three() {
+        assert!(is_even(&Permutation{ v: vec![0, 1, 2] }));
+        assert!(!is_even(&Permutation{ v: vec![0, 2, 1] }));
+        assert!(is_even(&Permutation{ v: vec![1, 2, 0] }));
+        assert!(!is_even(&Permutation{ v: vec![1, 0, 2] }));
+        assert!(is_even(&Permutation{ v: vec![2, 0, 1] }));
+        assert!(!is_even(&Permutation{ v: vec![2, 1, 0] }));
+    }
+}
